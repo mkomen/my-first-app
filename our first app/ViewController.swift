a1927300bf70ce426879967d03f11133df312e27
Michael Komendic <michaelkomendic@Michael-Komendics-MacBook-Pro.local>
2017-01-02 18:25:12 -0500

    Initial Commit

---
+++ our first app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  our first app
+//
+//  Created by Michael Komendic on 2017-01-02.
+//  Copyright © 2017 Michael Komendic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
